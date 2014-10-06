4302ea36af60a638f96c1f52a810b886ce454c3d
RR Arnold <rrarnold3@gmail.com>
2014-10-06 17:04:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/node --harmony
+
+var conf = require('./config.json')
+  , request = require('request')
+  , xmlParser = require('xml2json')
+  , GitHubEntry = require('./lib/GitHubEntry')
+
+
+const url = conf.url
+
+
+request.get(url, function(err, response, body) {
+  if (err) {
+    throw err; 
+  }
+  var json = xmlParser.toJson(response.body);
+  entries = JSON.parse(json).feed.entry;
+  entries.forEach(function(entry) {
+    gh = new GitHubEntry(entry)
+    if (gh.parseEntry().hasComment) {
+      // console.log('%s: %s', gh.author, gh.comment)
+    }
+  });
+});
